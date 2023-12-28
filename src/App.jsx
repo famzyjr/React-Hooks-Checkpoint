cf0cabe715c24c3e24d8ccc82b239c77fb030b6b
famzyjr <famuyiwaahmed132@gmail.com>
2023-12-28 17:14:40 +0100

    check

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MovieList from "./MovieList";
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+     <h1>Movies</h1>
+      <MovieList />
+    </>
+  )
+}
+
+export default App
---
+++ src/Filter.jsx
@@ -0,0 +1,14 @@
+
+
+ const Filter =  [
+
+    {
+        title: "Madam Koi Koi",
+        description: "all about bording school",
+        nationality: "Portugal",
+        rating: 90,
+        imageUrl: "https://images.unsplash.com/photo-1701636911624-637c97496874?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
+      },
+ ]
+
+export default Filter
